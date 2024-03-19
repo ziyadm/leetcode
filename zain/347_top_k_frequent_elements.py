f815d118352f3380e889ee72f457276ed8d1fae1
zkawoosa <zkawoosa@umich.edu>
2024-03-19 14:07:17 -0400

    Redid 347_top_k_frequent_elements

--- zain/347_top_k_frequent_elements.py
+++ zain/347_top_k_frequent_elements.py
@@ -1,41 +1,16 @@
+from collections import defaultdict
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        if len(nums) == 1:
-            return [nums[0]]
-        frequencies = {}
-        count = 0
-        while count <= len(nums):
-            frequencies[count] = []
-            count += 1
+        frequencies = defaultdict(int)
+        for num in nums:
+            frequencies[num] += 1
         
-        sorted_nums = sorted(nums)
-        left = 0
-        right = 1
-        current_count = 1
+        num_to_frequency = [(key, value) for key, value in frequencies.items()]
+        num_to_frequency.sort(key = lambda num_to_frequency: num_to_frequency[1], reverse=True)
+        
+        return [num for num, frequency in num_to_frequency[0:k]]
 
-        while right <= len(nums):
-            if right == len(nums):
-                frequencies[current_count].append(nums[left])
-                break
 
-            elif nums[right] == nums[left]:
-                current_count += 1
-                right += 1
-                
-            else:
-                frequencies[current_count].append(nums[left])
-                left = right
-                right = left + 1
-                current_count = 1
-
-        results = []
-        for frequency in range(len(nums), 0, -1):
-            if frequency in frequencies:
-                for number in frequencies[frequency]:
-                    results.append(number)
-                    if len(results) == k:
-                        return results
-        return results
 
 
 
