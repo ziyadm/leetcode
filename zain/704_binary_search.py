16311826a80b7c35ecfba835121a40a7e35f4cad
zkawoosa <zkawoosa@umich.edu>
2024-03-27 13:53:03 -0400

    Completed 704 binary search

---
+++ zain/704_binary_search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            middle = (left + right) // 2
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] > target:
+                right = middle - 1
+                middle = (left + right) // 2
+            else:
+                left = middle + 1
+                middle = (left + right) // 2  
+        return -1
